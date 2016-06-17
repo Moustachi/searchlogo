9990f662898808d62eea72c853e874e9cd6af421
Moustachi <durablehotpocket@gmail.com>
2016-06-16 19:27:12 -0700

    added functions to evaluate <select> values to highlight the correct logos, and reset function

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 $(function() {
+	$(".search").click(function(){
+		scholarshipHighschool();
+	});
 
-    $(".search").click(function(){
-    	scholarshipHighschool();
-    });
+	function scholarshipHighschool(){
+		if ($(".loan-menu").val() == "scholarship" &&
+			$(".school-menu").val() == "highschool"){
+			reset()
+			$(".scholarship-highschool").removeClass("gray");
+		} else if 
+			($(".loan-menu").val() == "loan" &&
+			$(".school-menu").val() == "k1"){
+			$(".loan-k1").removeClass("gray");
+		} else if 
+			($(".loan-menu").val() == "allProgram" &&
+			$(".school-menu").val() == "allSchool"){
+			reset()
+			$("img").removeClass("gray");
+		};
+	};
 
-    function scholarshipHighschool() {
-    	
-    	if ($(".loan-menu").val() == "scholarship" && 
-    		$(".school-menu").val() == "highschool"){
-    		$("#scholarship-highschoolgrey").css("display", "none")
-    		$("#scholarship-highschool").css("display", "inline-block")
-    	}
-    };
+	function reset(){
+		$("img").addClass("gray");
+	};
 });
